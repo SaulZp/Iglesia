275a4372f0fe32c630e7962f1996b53986526208
SaulZp <32599800+SaulZp@users.noreply.github.com>
2018-04-28 13:51:47 -0500

    Ultimos Cambios
    Cambios realizados antes de la presentacion

--- admin/index.php
+++ admin/index.php
@@ -86,11 +86,19 @@
                 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                         <h4 class="page-title">Inicio</h4> </div>
-                    
+                        
+                        
                     <!-- /.col-lg-12 -->
                 </div>
                                 <!--row -->
-                
+                <div class="row" style="background: url(img/index.jpg); background-size: cover; height: 400px; background-position: center;">
+                    <div class="col-md-12">
+                        <div class="text-center" style="vertical-align: center;">
+                            <h1 style="color: white;">Bienvenido</h1>
+                        </div>
+                    <!--<img src="img/index.jpg" ">-->
+                    </div>
+                </div>
                 
                 
             </div>
